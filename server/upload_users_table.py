636276a0bc3708b8f188d67d2b41da7013e3fe5e
Luca Biasotto <luca.biasotto@gmail.com>
2022-07-07 16:33:47 +0200

    Added test user

--- server/upload_users_table.py
+++ server/upload_users_table.py
@@ -1,4 +1,5 @@
 import sys, os
+import hashlib
 
 sys.path.insert(1, (str(sys.path[0]))+"/../../RankingService/")
 sys.path.insert(1, (str(sys.path[0]))+"/../../IGCrawlerService/crawler/")
@@ -13,12 +14,23 @@ def createUsersDatabase():
 	db = DBConnection()
 
 	db.createDatabaseConnection("MichelinSocial")
+
 	query = """CREATE TABLE IF NOT EXISTS Users(
 		Email VARCHAR(50) PRIMARY KEY,
 		Username VARCHAR(20) NOT NULL,
-		Password VARCHAR(20) NOT NULL,
+		Password VARCHAR(100) NOT NULL,
 		Admin tinyint(1)
 	)"""
+
+	db.executeQuery(query)
+
+	mail = '" test.ecample@gmail.com", '
+	name = '"Test Name", '
+	password = 'TestPassword'
+	password = '"' + hashlib.sha256(password.encode('utf-8')).hexdigest() + '", '
+	admin = '"0"'
+	query = "INSERT INTO Users VALUES (" + mail + name + password + admin + ');'
+
 	db.executeQuery(query)
 
 	db.uploadDB("MichelinSocial", (str(sys.path[0]))+"/../../RankingService/")
